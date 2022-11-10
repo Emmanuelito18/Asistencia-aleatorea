77b8aca56065ef8fedd56681b2d6b247bb192cfc
Emmanuelito18 <holasoygermanyolo@gmail.com>
2022-11-10 02:22:35 -0600

    creación de archivos con sln

---
+++ Asistencia aleatorea/Persona.cpp
@@ -0,0 +1,17 @@
+#include "Persona.h"
+
+/*Constructor*/
+Persona::Persona(string aPaterno,string aMaterno, string _nombre) {
+	apellidoPaterno = aPaterno;
+	apellidoMaterno = aMaterno;
+	nombre = _nombre;
+}
+
+/*Destructor*/
+Persona::~Persona() {
+
+}
+ 
+/*virtual*/ void Persona::mostrarInfo() {
+
+}
---
+++ Asistencia aleatorea/Persona.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Persona{
+	protected://Atributos
+		string apellidoPaterno;
+		string apellidoMaterno;
+		string nombre;
+	public://Métodos
+		Persona(string,string,string);//Constructor
+		~Persona();//Destructor
+		/*virtual*/ void mostrarInfo();
+};
+
