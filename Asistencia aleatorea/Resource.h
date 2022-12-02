8a86d46ca5e296d7702b915bebaad6c86d986482
Emmanuelito18 <holasoygermanyolo@gmail.com>
2022-12-02 02:17:27 -0600

    Se añadieron 5 botones a la ventana principal, se cambio el nombre y se añadieron nuevos menús, se camió el texto al dialogo "Acerca de..."

--- Asistencia aleatorea/Resource.h
+++ Asistencia aleatorea/Resource.h
@@ -1,30 +1,35 @@
 //{{NO_DEPENDENCIES}}
-// Archivo de inclusiÃ³n generado de Microsoft Visual C++.
+// Archivo de inclusión generado de Microsoft Visual C++.
 // Usado por Asistencia aleatorea.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_ASISTENCIAALEATOREA_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_ASISTENCIAALEATOREA			107
-#define IDI_SMALL				108
-#define IDC_ASISTENCIAALEATOREA			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Valores predeterminados siguientes para nuevos objetos
 //
+#define IDC_MYICON                      2
+#define IDD_ASISTENCIAALEATOREA_DIALOG  102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_ASISTENCIAALEATOREA         107
+#define IDI_SMALL                       108
+#define IDC_ASISTENCIAALEATOREA         109
+#define IDR_MAINFRAME                   128
+#define ID_ARCHIVO_CREARGRUPO           32771
+#define ID_ARCHIVO_EDITARGRUPO          32772
+#define ID_ARCHIVO_MOSTRARGRUPO         32773
+#define ID_ARCHIVO_GENERARALUMNOS       32774
+#define ID_ARCHIVO_SEPARADOR            32775
+#define ID_ARCHIVO_SEPARADOR32776       32776
+#define ID_AYUDA_MANUAL                 32777
+#define ID_AYUDA_SEPARADOR              32778
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
